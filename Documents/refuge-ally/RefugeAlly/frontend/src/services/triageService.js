df8cb659dc4621861c8680e0a43d60e440552123
abjt01 <abjty02@gmail.com>
2025-09-19 11:47:41 +0530

    Delete Documents/refuge-ally/RefugeAlly directory

--- Documents/refuge-ally/RefugeAlly/frontend/src/services/triageService.js
+++
@@ -1,9 +0,0 @@
-import api from './api';
-
-const triageService = {
-  submitSymptoms: async (symptomsData) => {
-    return await api.post('/api/triage', symptomsData);
-  }
-};
-
-export default triageService;
